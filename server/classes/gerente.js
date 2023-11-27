7cc9963c813ef3605a9eaeea6039df6515e53e25
Mateus Braga <mateus.braga@academico.ifpb.edu.br>
2023-11-26 23:55:58 -0300

    strategy v1

--- server/classes/gerente.js
+++ server/classes/gerente.js
@@ -7,16 +7,34 @@ const MensalBuilder = require('./planoBuilder')
 const TrimestralBuilder = require('./planoBuilder')
 const SemestralBuilder = require('./planoBuilder')
 const AnualBuilder = require('./planoBuilder')
+const {Cadastro} = require('./strategy')
+const {CadastroPlano} = require('./strategy')
+const {CadastroModalidade} = require('./strategy')
+const {CadastroFuncionarios} = require('./strategy')
 
 class Gerente extends Atendente{
     facadeFinanceiro = new FacadeFinanceiro()
     planoBuilder = new PlanoBuilder()
+    cadastroFuncionarios = new CadastroFuncionarios()
+    cadastroPlano = new CadastroPlano()
+    cadastroModalidade = new CadastroModalidade()
+    cadastro = new Cadastro()
 
-    // constructor(builder){
-    //     this.builder = builder
-    // }
+    async createFuncionario(funcionario){
+        this.cadastro.setServico(this.cadastroFuncionarios)
+        this.cadastro.cadastrar(funcionario)
+    }
 
+    async createModalidade(modalidade){
+        this.cadastro.setServico(this.cadastroModalidade)
+        this.cadastro.cadastrar(modalidade)
+    }
 
+    async createPlano(plano) {
+        this.cadastro.setServico(this.cadastroPlano)
+        this.cadastro.cadastrar(plano)
+    }
+    
     async construirPlano(plano){
         await this.builder.buildTipo(plano.tipo)
         await this.builder.buildPacote()
@@ -34,145 +52,6 @@ class Gerente extends Atendente{
         return dados
     }
     
-    async createFuncionario(funcionario){
-        const pessoaId = randomUUID()
-        
-        async function inserir(){
-            await queryDB({
-                query: 'insert into pessoa (idpessoa, nome, sexo, nascimento, cpf, telefone, email, senha) values (?, ?, ?, ?, ?, ?, ?, ?)',
-                values: [
-                    pessoaId, 
-                    funcionario.nome, 
-                    funcionario.sexo, 
-                    funcionario.nascimento, 
-                    funcionario.cpf, 
-                    funcionario.telefone, 
-                    funcionario.email, 
-                    funcionario.senha
-                ]
-            })
-            queryDB({
-                query: "INSERT INTO funcionario (cargo, salario, dataAdmissao, dataPagamento, pessoa_idpessoa) VALUES (?, ?, ?, ?, ?)",
-                values: [
-                    funcionario.cargo,
-                    funcionario.salario,
-                    funcionario.dataAdmissao,
-                    funcionario.dataPagamento,
-                    pessoaId
-                ]
-            })
-        }
-        inserir()
-    }
-
-    async createModalidade(modalidade){
-        async function inserir(){
-            await queryDB({
-                query: 'INSERT INTO modalidade (nome, descricao) VALUES (?, ?)',
-                values: [
-                    modalidade.nome,
-                    modalidade.descricao
-                ]
-            })
-        }
-        inserir()
-    }
-
-    async createPlano(plano) {
-        const pacoteM = 'Mensal';
-        const pacoteT = 'Trimestral';
-        const pacoteS = 'Semestral';
-        const pacoteA = 'Anual';
-    
-        const tipo = plano.tipo;
-        const valor = plano.valor;
-        const desconto = plano.desconto;
-        const modalidades = plano.modalidades;
-    
-        async function inserir(pacote, meses, multiplicador, idmodalidades) {
-            const valorCalculado = (valor * meses) * (1 - (desconto * multiplicador / 100));
-    
-            const {insertId} = await queryDB({
-                query: `
-                    INSERT INTO plano (tipo, pacote, valor)
-                    VALUES (?, ?, ?);
-                `,
-                values: [tipo, pacote, valorCalculado],
-            });
-
-            for(let i = 0; i < idmodalidades.length; i++){
-                await queryDB({
-                    query: `
-                        INSERT INTO vinc_plano_modalidade (plano_idplano, modalidade_idmodalidade)
-                        VALUES (?, ?);
-                    `,
-                    values: [insertId, idmodalidades[i]],
-                });
-            }
-        }
-    
-        try {
-            await inserir(pacoteM, 1, 0, modalidades);
-            await inserir(pacoteT, 3, 1, modalidades);
-            await inserir(pacoteS, 6, 2, modalidades);
-            await inserir(pacoteA, 12, 3, modalidades);
-    
-            console.log('Inserções concluídas com sucesso');
-        } catch (error) {
-            console.error('Erro ao inserir dados no banco de dados:', error);
-            throw error; // Rejeita a promise para indicar falha na execução da função
-        }
-    }
-
-    async editarFuncionario(funcionario){     
-        const idPessoa = funcionario.idpessoa
-
-        async function inserir(){
-            await queryDB({
-                query: `UPDATE pessoa
-                        SET
-                            nome = ?,
-                            sexo = ?,
-                            nascimento = ?,
-                            cpf = ?,
-                            email = ?,
-                            telefone = ?,
-                            senha = ?
-                        WHERE
-                            idpessoa = ?;
-                    `,
-                values: [
-                    funcionario.nome, 
-                    funcionario.sexo, 
-                    funcionario.nascimento, 
-                    funcionario.cpf, 
-                    funcionario.email, 
-                    funcionario.telefone, 
-                    funcionario.senha,
-                    idPessoa
-                ]
-            })
-            queryDB({
-                query: `UPDATE funcionario
-                        SET
-                            cargo = ?,
-                            salario = ?,
-                            dataAdmissao = ?,
-                            dataPagamento = ?
-                        WHERE
-                            pessoa_idpessoa = ?;
-                    `,
-                values: [
-                    funcionario.cargo,
-                    funcionario.salario,
-                    funcionario.dataAdmissao,
-                    funcionario.dataPagamento, 
-                    idPessoa 
-                ]
-            })
-        }
-        inserir()
-    }
 }
 
 module.exports = Gerente
